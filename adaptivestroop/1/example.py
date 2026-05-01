d9f9ff808dafc9eb0d9e30e3dfe979398974f9bb
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2026-04-30 23:10:03 -0700

    automatic update

--- adaptivestroop/1/example.py
+++ adaptivestroop/1/example.py
@@ -5,11 +5,15 @@ win=visual.Window(units= "pix", size=(1000, 1000), color=[-1,-1,-1], fullscr = F
 text = visual.TextStim(win,"Welcome to the experiment", height = 40, pos = (0,0))
 text.draw()
 win.flip()
-core.wait(3)
+core.wait(1)
 
 rectangle = visual.Rect(win, width=100, height=100,fillColor=[1,1,1], pos = (0, 300))
 rectangle.draw()
+win.flip()
+core.wait(1)
+
 text.draw()
+rectangle.draw()
 win.flip()
 core.wait(1)
 
